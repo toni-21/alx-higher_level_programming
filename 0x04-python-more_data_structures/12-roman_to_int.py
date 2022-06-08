61f9ddd26d9be5d818dc747b58e00e5f5ab7c3c3
Toni <dannyore001@gmail.com>
2022-06-08 07:23:35 -0700

    fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if roman_string is None or type(roman_string) == str:
+        return (0)
+
     roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
     i = 0
     num = 0
@@ -11,4 +14,4 @@ def roman_to_int(roman_string):
             #print(i)
             num+=roman[roman_string[i]]
             i+=1
-    return num
+    return (num)
